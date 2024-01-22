39bf53e03f3c9b6d812a3b0634d0a6bb78c2cd52
albertoaguilera31 <123708306+albertoaguilera31@users.noreply.github.com>
2024-01-21 23:53:49 -0300

    se añadieron router y cambios en las rutas para filtrar elementos de un objeto

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const app = express();
 
 const {infoCursos}= require('./cursos.js');
 
+//ROUTERS
+const routerProgramacion= express.Router();
+app.use('/api/cursos/programacion', routerProgramacion);
+
+
 
 //routing= conocido como direccionamiento o enrutamiento
 
@@ -14,16 +19,64 @@ app.get('/api/cursos', (req, res)=>{
     res.send(JSON.stringify(infoCursos));
 });
 
-app.get('/api/cursos/programacion', (req, res)=>{
+//CURSOS DE PROGRAMACION
+routerProgramacion.get('/', (req, res)=>{
     res.send(JSON.stringify(infoCursos.programacion));
 })
+
+app.get('/api/cursos/programacion/:lenguaje', (req, res)=>{
+    const lenguaje= req.params.lenguaje;
+    const resultados= infoCursos.programacion.filter(curso=> curso.lenguaje === lenguaje);
+
+    if(resultados.length ===0){
+        return res.status(404).send(`No se encontraron cursos de ${lenguaje}`);
+    }
+
+    console.log(req.query.ordenar);
+
+    res.send(JSON.stringify(resultados));
+});
+
+app.get('/api/cursos/programacion/:lenguaje/:nivel', (req,res)=>{
+    const lenguaje= req.params.lenguaje;
+    const nivel= req.params.nivel;
+
+    const resultados= infoCursos.programacion.filter(curso=> curso.lenguaje ===lenguaje && curso.nivel=== nivel);
+
+    if(resultados.length ===0){
+        return res.status(404).send(`No se encontraron cursos de ${lenguaje} de nivel ${nivel}`);
+    }
+    
+    res.send(JSON.stringify(resultados));
+})
+
+//CURSOS DE MATEMATICAS
 
 app.get('/api/cursos/matematicas', (req, res)=>{
     res.send(JSON.stringify(infoCursos.matematicas));
 })
+
+app.get('/api/cursos/matematicas/:tema', (req, res)=>{
+    const tema= req.params.tema;
+    const resultados= infoCursos.matematicas.filter(curso=> curso.tema === tema);
+
+    if(resultados.length ===0){
+        return res.status(404).send(`No se encontró el tema ${tema}`);
+    }
+    
+    res.send(JSON.stringify(resultados));
+});
+
 
 const PUERTO= process.env.PORT || 3000;
 
 app.listen(PUERTO, ()=>{
     console.log(`El servidor se esta ejecutando en el puerto ${PUERTO}...`)
 })
+
+
+
+
+
+
+//});
